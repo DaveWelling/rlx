cff96c63657ab0ac20b371c11c2e8c3b9fe92f83
David Welling <david.welling@sstid.com>
2021-06-02 10:23:44 -0400

    remove old components

--- rlx_components/src/components/formElements/index.js
+++
@@ -1,6 +0,0 @@
-//Call_[npm_run_updateIndexes]_to_update_this_file.
-
-// @create-index
-
-export { default as DropDown } from './DropDown';
-export { default as ShortText } from './ShortText';
--- rlx_components/src/hooks/useActiveRecord.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { ActiveRecordContext } from '../components/ActiveRecord';
-
-export default function useActiveRecord() {
-    return useContext(ActiveRecordContext);
-}
