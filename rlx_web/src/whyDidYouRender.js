b591e130ae2d7f6b256f9ca9ba1e88b1df86a786
David Welling <david.welling@sstid.com>
2021-06-07 12:40:27 -0400

    remove all pure components from whydidyourender to avoid avoid problem with menus

--- rlx_web/src/whyDidYouRender.js
+++ rlx_web/src/whyDidYouRender.js
@@ -1,5 +1,3 @@
 import React from 'react';
 import whyDidYouRender from '@welldone-software/why-did-you-render';
-whyDidYouRender(React, {
-    trackAllPureComponents: true
-});
+whyDidYouRender(React, {});
